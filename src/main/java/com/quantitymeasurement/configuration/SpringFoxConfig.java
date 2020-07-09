5cf7387182a854f1441dd4b1df77bf5695015cf5
madhuvayya <madhu.vayya@gmail.com>
2020-07-10 01:09:27 +0530

    Refactored SpringFoxConfig class

--- src/main/java/com/quantitymeasurement/configuration/SpringFoxConfig.java
+++ src/main/java/com/quantitymeasurement/configuration/SpringFoxConfig.java
@@ -2,7 +2,9 @@ package com.quantitymeasurement.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,9 +18,20 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.quantitymeasurement"))
                 .paths(regex("/quantity-measurement.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Quantity Measurement API")
+                .description("API for converting units")
+                .contact("madhu.vayya@gmail.com")
+                .license("JavaInUse License")
+                .licenseUrl("javainuse@gmail.com")
+                .version("1.0")
                 .build();
     }
 
